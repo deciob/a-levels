477b42367e243ab092e27dca030dc754eff6e2c5
deciob <decio.battaglia@gmail.com>
2014-07-26 17:44:21 +0100

    removed no longer used fiels

--- app/scripts/services/string_service.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-app.service('slugifyNameService', function() {
-
-  this.slugify = function(name) {
-    return name.replace(/ - /g, ' ').replace(/ /g, '_').toLowerCase();
-  }
-    
-});
